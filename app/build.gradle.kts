c55efea2e8b1daf89a4f8f7e128f92f34865cba1
Aswin C <aswinanand9020@gmail.com>
2026-04-12 01:17:12 +0530

    chore: bump version to 1.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "cx.aswin.boxcast"
         minSdk = 31
         targetSdk = 36
-        versionCode = 22
-        versionName = "1.3.4"
+        versionCode = 23
+        versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
